c424e60593cb2ecb30ada2292e2c737469846942
Zoe Bachman <zoe.bachman@gmail.com>
2017-02-14 00:15:24 -0500

    working api code, night and day mode

--- htp_tutorial/sketch.js
+++ htp_tutorial/sketch.js
@@ -1,7 +1,114 @@
+var weatherData;
+//var boop = 0;
+var dimYellow = (255, 255, 204);
+var brightYellow = (255, 255, 0);
+
+var cnv;
+var d;
+var blueBack = 255;
+var yell;
+var thickness;
+// var windowW = 255;
+// var windowH = 255;
+
+
+var humidity;
+var input;
+var button;
+
+//construct url
+var url = "http://api.openweathermap.org/data/2.5/weather?zip=";
+//var url = "http://api.openweathermap.org/data/2.5/find?q=";
+//var zip = "11222" // eventually: input.value();
+var countryCode = ",us";
+var metric = "&units=metric";
+var API = "&appid=2585c2354e7209577fbcb9a5ad1c9367";
+
+//"http://api.openweathermap.org/data/2.5/find?q=London&units=metric&appid=2585c2354e7209577fbcb9a5ad1c9367
+
+var unixTime = 'http://www.convert-unix-time.com/api?timestamp=now'
+var timeData;
+var time;
+var sunset; 
+var sunrise;
+
 function setup() {
+  createCanvas(400, 400);
   
+  button = select("#submit");
+  button.mousePressed(weatherAsk);
+  input = select('#zip')
+  
+  refreshButton = select("#refresh")
+  refreshButton.mousePressed(resetSketch)
 }
 
-function draw() {
+function weatherAsk() {
+  var call = url + input.value() + metric + API;
+  loadJSON(call, gotData, 'jsonp');
+  //loadJSON(unixTime, gotTimeData)//, 'jsonp');
+}
+
+function resetSketch(){
+  background("white")
+}
+
+// function gotTimeData(data){
+//   timeData = data;
+//   console.log(timeData.timestamp);
+// }
+
+function convertUnixTimeCallback(result){
+    time = result.timestamp;
+    console.log(result.timestamp);
+}
+
+function gotData(data) {
+
+  weatherData = data;
+  console.log(weatherData.sys.sunset);
+  sunset = weatherData.sys.sunset;
+  sunrise = weatherData.sys.sunset;
+  //convert sunset into real time, see if it is between certain times for it to be light or dark
+  //console.log(weatherData.main.humidity)
+  var humidityColor = weatherData.main.humidity;
   
+  if (sunrise < time && time < sunset){
+    //it's day
+    background(humidityColor, 195, 255, humidityColor);
+  } else {
+    //it's night
+    background(humidityColor, 10, 10);
+  }
+
+  
+  //text description
+  // console.log(weatherData.weather[0].main);
+  fill("white");
+  text(weatherData.name, 10, 20)
+  text(weatherData.weather[0].main, 10, 40)
+  text(weatherData.weather[0].description, 10, 50)
+
+  //temp will be rays
+  //console.log(weatherData.main.temp);
+  var temp = weatherData.main.temp; //yellow color
+  fill(255, 255, temp);
+  //ellipse(100, 100, temp*100, temp*100 )
+
+  //first, map sunlight values
+  //var density = map(amount of sunlight, 0, width, 20, 50)
+ // var density = map(temp, 0, height, 20, 50); //
+  translate(width / 2, height / 2); //lines drawn from center
+  
+  ellipse(0,0,temp,temp)
+  
+  
+  // for (var x = 0; x <= width + 500; x += density) {
+  //   for (var y = 10; y <= height + 500; y += density) {
+  //     //rotate((PI / 90));
+  //     rect(x, y, 5, 500)
+  //   }
+  // }
+  
+
 }
