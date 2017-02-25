18ea362a39093359c73d1f0dec5ba90f813b34a7
Zoe Bachman <zoe.bachman@gmail.com>
2017-02-24 22:30:38 -0500

    updated README

--- HTP_tutorial_DOM/sketch.js
+++ HTP_tutorial_DOM/sketch.js
@@ -1,15 +1,11 @@
+var input;
+var button;
+
 //construct url
 var url = "http://api.openweathermap.org/data/2.5/weather?zip=";
-//var url = "http://api.openweathermap.org/data/2.5/find?q=";
-var zip = "11222" // eventually: input.value(); //or randomly select from a list of zipcodes
 var countryCode = ",us";
 var metric = "&units=metric";
 var API = "&appid=2585c2354e7209577fbcb9a5ad1c9367";
-
-
-// http://api.openweathermap.org/data/2.5/weather?zip=11222,us&units=metric&appid=2585c2354e7209577fbcb9a5ad1c9367
-
-//"http://api.openweathermap.org/data/2.5/find?q=London&units=metric&appid=2585c2354e7209577fbcb9a5ad1c9367
 
 var unixTime = 'http://www.convert-unix-time.com/api?timestamp=now'
 var timeData;
@@ -19,28 +15,40 @@ var sunrise;
 var weatherData;
 var humidity;
 
+
 function setup() {
   createCanvas(400, 400);
-  var call = url + zip + metric + API;
-   console.log(call)
-   loadJSON(call, gotData, 'jsonp');
+
+  button = select("#submit");
+  button.mousePressed(weatherAsk);
+  input = select('#zip')
+
+  
+  refreshButton = select("#refresh")
+  refreshButton.mousePressed(resetSketch)
+}
+
+function weatherAsk() {
+  var call = url + input.value() + metric + API;
+  loadJSON(call, gotData, 'jsonp');
+}
+
+function resetSketch() {
+  background("white")
 }
 
 function convertUnixTimeCallback(result) {
   time = result.timestamp;
-
 }
 
 function gotData(data) {
 
   weatherData = data;
-  
-  //console.log(weatherData.sys.sunset);
+
   sunset = weatherData.sys.sunset;
-  sunrise = weatherData.sys.sunrise;
+  sunrise = weatherData.sys.sunset;
   
   //convert sunset into real time, see if it is between certain times for it to be light or dark
-  //console.log(weatherData.main.humidity)
   var humidityColor = weatherData.main.humidity;
 
   if (sunrise < time || time < sunset) {
@@ -71,25 +79,25 @@ function gotData(data) {
   }
   pop()
   
+  //add overlay for clouds/rain description
   if (weatherData.weather[0].main == "Clouds" || weatherData.weather[0].main == "Rain"){
     fill(0,0,0,100)
     rect(0, 0, 400, 400)
   } else{
     noFill();
-    // rect(0, 0, 400, 400)
   }
   
   //text description
-  // console.log(weatherData.weather[0].main);
- noStroke();
+  noStroke();
   fill(0,0,0,100)
   rect(0, 360, 400, 40)
   fill("#32CD32");
-  //fill("white");
-  stroke(50);
+  stroke(2);
   textSize(20);
   text(weatherData.name, 10, 390)
   text(weatherData.weather[0].main, 180, 390)
   //text(weatherData.weather[0].description, 250, 380)
   text(temp, 300, 390)
+
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # htp_tutorial2017
-Repo for HTP Tutorial, Spring 2017
+Repo for [HTP Tutorial](http://www.hackthepatriarchybaltimore.com/), Spring 2017
+
+This p5.js sketch is a weather visualizer that uses data pulled from the openweatherAPI.  I like how working with changing data can effect a sketch (see the beautiful [wind map] (http://hint.fm/wind/)visualization).There’s a basic version with two variations so you can play around with the interface, making use of the DOM or separate data files. I made this because
+
+## Includes *three* versions of an openweatherAPI controlled drawing:
+
+1. Basic (pre-populated zip code)
+2. DOM version (user can enter zip codes and refresh the canvas)
+3. Random zip version (uses an extra list of zip codes and randomly chooses one each time it’s refreshed)
+
+## Sources:
+To make this sketch, I relied on the [p5.js reference page](http://p5js.org/reference/), Dan Shiffman’s [coding train videos](https://www.youtube.com/user/shiffman) - great introduction/refresher to working with data in various forms like API and JSON.
+
+I also looked at example code from my [Introduction to Computational Media class] (https://github.com/ITPNYU/ICM-2015/) at NYU-ITP.
+
+…I specifically looked at [this sketch](https://github.com/ITPNYU/ICM-2015/blob/master/03_interaction/for_loops5/sketch.js) when looking for inspiration  
+ 
+To do the tutorial, sign up for an account with [open weather data](http://openweathermap.org/) and get your API key.
+
+
+
+
